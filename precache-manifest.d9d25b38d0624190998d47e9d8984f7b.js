5a66d77ed47528f4c54f619c503c049a3563822f
swapnasubham das <swapnasubham.das@protonmail.com>
2020-09-02 02:02:05 +0530

    Updates

---
+++ precache-manifest.d9d25b38d0624190998d47e9d8984f7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6095e371047c57fffbf95774b8a9283",
+    "url": "/friends/index.html"
+  },
+  {
+    "revision": "4016fac7fce10e065bce",
+    "url": "/friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ac58e25bdaeb0f5ee022",
+    "url": "/friends/static/css/main.4e046319.chunk.css"
+  },
+  {
+    "revision": "4016fac7fce10e065bce",
+    "url": "/friends/static/js/2.a6e8b5b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friends/static/js/2.a6e8b5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac58e25bdaeb0f5ee022",
+    "url": "/friends/static/js/main.4c94200c.chunk.js"
+  },
+  {
+    "revision": "006a80e2e8909bb5a49b",
+    "url": "/friends/static/js/runtime-main.df665f4e.js"
+  },
+  {
+    "revision": "3eb2be46a75f69a75ac6142a350c062c",
+    "url": "/friends/static/media/Patchy Robots.3eb2be46.woff"
+  }
+]);
